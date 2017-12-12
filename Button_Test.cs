6aa70e575501900076ac90a892dc1436bb319c8c
DESKTOP-7T13ST5\suzuki <suzuki014t@gmail.com>
2017-12-13 00:14:46 +0900

    ファイル追加。ボタンの選択処理、選択したボタンの番号の遷移、シーンの遷移

---
+++ Button_Test.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Button_Test : MonoBehaviour {
+
+    SpriteRenderer MainSpriteRenderer;
+
+    public Sprite btn05;
+    private GameObject obj;
+    private Image image;
+    public Sprite HoldSprite;
+
+    public static int kintreCnt = 0;
+
+    void Start()
+    {
+    }
+
+    public void Button1Click()
+    {
+        Debug.Log("Button1 Click");
+        kintreCnt = 5; // 変数を次のシーンに持たせる
+        getHitPoint();
+        //MainSpriteRenderer.sprite = HoldSprite;
+
+        //buttonImage_.sprite = "set5";
+
+        //obj = GameObject.Find("set5").gameObject as GameObject;
+        //image = obj.GetComponent<Image>();
+    }
+
+    public static int getHitPoint()
+    {
+        return kintreCnt;
+    }
+
+    public void ButtonClick10()
+    {
+        Debug.Log("ButtonClick10 Click");
+        kintreCnt = 10; // 変数を次のシーンに持たせる
+        getHitPoint();
+        //MainSpriteRenderer.sprite = HoldSprite;
+
+        //buttonImage_.sprite = "set5";
+
+        //obj = GameObject.Find("set5").gameObject as GameObject;
+        //image = obj.GetComponent<Image>();
+    }
+
+    public void ButtonClick15()
+    {
+        kintreCnt = 15; // 変数を次のシーンに持たせる
+        getHitPoint();
+        Debug.Log("ButtonClick15 Click");
+
+    }
+
+    public void ButtonClick20()
+    {
+        kintreCnt = 20; // 変数を次のシーンに持たせる
+        getHitPoint();
+        Debug.Log("ButtonClick20 Click");
+
+    }
+
+    public void ButtonClick25()
+    {
+        kintreCnt = 25; // 変数を次のシーンに持たせる
+        getHitPoint();
+        Debug.Log("ButtonClick25 Click");
+
+    }
+
+    // スクワット開始
+    public void startSquwat()
+    {
+        Debug.Log("startSquwat");
+
+        SceneManager.LoadScene("squwat");
+    }
+
+}
---
+++ Squwat.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Squwat : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        int resultHitpoint = Button_Test.getHitPoint();
+        Debug.Log(resultHitpoint);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
