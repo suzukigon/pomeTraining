efc7c00d341a0ce504bd53467c3614dbade8a81a
DESKTOP-7T13ST5\suzuki <suzuki014t@gmail.com>
2017-12-16 01:16:19 +0900

    スクワットと腕立ての選択機能を作成🐶

---
+++ chgSprite.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class chgSprite : MonoBehaviour {
+
+    public Sprite spriteMae;
+    public Sprite spriteAto;
+
+    public Sprite udeMae;
+    public Sprite udeAto;
+
+    private bool chFlg = false;
+    private GameObject ude;
+    private GameObject sqw;
+
+    public void changeSprite() // スクワットをタップした時
+    {
+        // 画像クリックで切り替える
+        ude = GameObject.Find("Udetate");
+        this.gameObject.GetComponent<Image>().sprite = spriteMae;
+        ude.gameObject.GetComponent<Image>().sprite = udeMae;
+    }
+
+    public void changeSpriteUde() // 腕立てをタップした時
+    {
+        // 画像クリックで切り替える
+        sqw = GameObject.Find("squwat");
+        this.gameObject.GetComponent<Image>().sprite = udeAto;
+        sqw.gameObject.GetComponent<Image>().sprite = spriteAto;
+    }
+}
