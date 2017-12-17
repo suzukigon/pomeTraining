9e83aabbb9659e086478bca880233b4b9f033a43
DESKTOP-7T13ST5\suzuki <suzuki014t@gmail.com>
2017-12-17 17:50:24 +0900

    回数選択時の挙動を作成。画面間の変数の遷移を追加😼

--- Button_Test.cs
+++ Button_Test.cs
@@ -4,80 +4,113 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
+/*
+* メニュー画面の筋トレ回数選択の取得
+* スクワット / 腕立て　の切り替え
+*/
+
 public class Button_Test : MonoBehaviour {
 
     SpriteRenderer MainSpriteRenderer;
 
-    public Sprite btn05;
-    private GameObject obj;
-    private Image image;
-    public Sprite HoldSprite;
-
     public static int kintreCnt = 0;
 
-    void Start()
+    public Sprite btn05Mae;
+    public Sprite btn05Ato;
+    public Sprite btn10Mae;
+    public Sprite btn10Ato;
+    public Sprite btn15Mae;
+    public Sprite btn15Ato;
+    public Sprite btn20Mae;
+    public Sprite btn20Ato;
+    public Sprite btn25Mae;
+    public Sprite btn25Ato;
+
+    private GameObject obj;
+    private Image image;
+
+    private GameObject cnt05;
+    private GameObject cnt10;
+    private GameObject cnt15;
+    private GameObject cnt20;
+    private GameObject cnt25;
+
+    private GameObject sqw;
+    private GameObject udetate;
+
+    public void resetBtn() // ボタン選択の初期化
     {
+        cnt05 = GameObject.Find("Button05");
+        cnt05.gameObject.GetComponent<Image>().sprite = btn05Mae;
+        cnt10= GameObject.Find("Button10");
+        cnt10.gameObject.GetComponent<Image>().sprite = btn10Mae;
+        cnt15 = GameObject.Find("Button15");
+        cnt15.gameObject.GetComponent<Image>().sprite = btn15Mae;
+        cnt20 = GameObject.Find("Button20");
+        cnt20.gameObject.GetComponent<Image>().sprite = btn20Mae;
+        cnt25 = GameObject.Find("Button25");
+        cnt25.gameObject.GetComponent<Image>().sprite = btn25Mae;
     }
 
-    public void Button1Click()
-    {
-        Debug.Log("Button1 Click");
-        kintreCnt = 5; // 変数を次のシーンに持たせる
-        getHitPoint();
-        //MainSpriteRenderer.sprite = HoldSprite;
-
-        //buttonImage_.sprite = "set5";
-
-        //obj = GameObject.Find("set5").gameObject as GameObject;
-        //image = obj.GetComponent<Image>();
-    }
-
-    public static int getHitPoint()
+    public static int getHitPoint() // 選択した回数をトレーニングシーンに渡す
     {
         return kintreCnt;
     }
 
+    public void Button1Click5()
+    {
+        kintreCnt = 5; // 変数を次のシーンに持たせる
+        resetBtn();
+        this.gameObject.GetComponent<Image>().sprite = btn05Ato;
+        getHitPoint();
+
+        //データ取得
+        int total_count_sqw = PlayerPrefs.GetInt("TotalCountSqw");
+        total_count_sqw = total_count_sqw + 5;
+
+        //データ入力
+        PlayerPrefs.SetInt("TotalCountSqw", total_count_sqw);
+        
+    }
+
     public void ButtonClick10()
     {
-        Debug.Log("ButtonClick10 Click");
+        resetBtn();
         kintreCnt = 10; // 変数を次のシーンに持たせる
+        this.gameObject.GetComponent<Image>().sprite = btn10Ato;
         getHitPoint();
-        //MainSpriteRenderer.sprite = HoldSprite;
-
-        //buttonImage_.sprite = "set5";
-
-        //obj = GameObject.Find("set5").gameObject as GameObject;
-        //image = obj.GetComponent<Image>();
     }
 
     public void ButtonClick15()
     {
         kintreCnt = 15; // 変数を次のシーンに持たせる
+        resetBtn();
+        this.gameObject.GetComponent<Image>().sprite = btn15Ato;
         getHitPoint();
-        Debug.Log("ButtonClick15 Click");
-
     }
 
     public void ButtonClick20()
     {
         kintreCnt = 20; // 変数を次のシーンに持たせる
+        resetBtn();
+        this.gameObject.GetComponent<Image>().sprite = btn20Ato;
         getHitPoint();
-        Debug.Log("ButtonClick20 Click");
-
     }
 
     public void ButtonClick25()
     {
         kintreCnt = 25; // 変数を次のシーンに持たせる
         getHitPoint();
-        Debug.Log("ButtonClick25 Click");
-
+        resetBtn();
+        this.gameObject.GetComponent<Image>().sprite = btn25Ato;
     }
 
     // スクワット開始
     public void startSquwat()
     {
-        Debug.Log("startSquwat");
+        int resultHitpoint = chgSprite.getTrn();
+        Debug.Log(resultHitpoint);
+
 
         SceneManager.LoadScene("squwat");
     }
--- chgSprite.cs
+++ chgSprite.cs
@@ -5,6 +5,8 @@ using UnityEngine.UI;
 
 public class chgSprite : MonoBehaviour {
 
+    public static int trnKind = 0;
+
     public Sprite spriteMae;
     public Sprite spriteAto;
 
@@ -15,12 +17,20 @@ public class chgSprite : MonoBehaviour {
     private GameObject ude;
     private GameObject sqw;
 
+    public static int getTrn()
+    {
+        return trnKind;
+    }
+
     public void changeSprite() // スクワットをタップした時
     {
         // 画像クリックで切り替える
         ude = GameObject.Find("Udetate");
         this.gameObject.GetComponent<Image>().sprite = spriteMae;
         ude.gameObject.GetComponent<Image>().sprite = udeMae;
+
+        trnKind = 0; // スタート開始時に渡すフラグ
+        getTrn();
     }
 
     public void changeSpriteUde() // 腕立てをタップした時
@@ -29,5 +39,8 @@ public class chgSprite : MonoBehaviour {
         sqw = GameObject.Find("squwat");
         this.gameObject.GetComponent<Image>().sprite = udeAto;
         sqw.gameObject.GetComponent<Image>().sprite = spriteAto;
+
+        trnKind = 1; // スタート開始時に渡すフラグ
+        getTrn();
     }
 }
